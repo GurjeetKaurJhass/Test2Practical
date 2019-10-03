b955ef6a5e0de69830d691fb47be8fe6f597b94e
gurjeet <gurjit.babrah@gmail.com>
2019-10-03 12:11:18 -0400

    Dinosaur is moving up and down on the screen

---
+++ app/src/main/java/com/example/tappyspaceship01/GameEngine.java
@@ -0,0 +1,207 @@
+package com.example.tappyspaceship01;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class GameEngine extends SurfaceView implements Runnable {
+
+    // Android debug variables
+    final static String TAG="DINO-RAINBOWS";
+
+    // screen size
+    int screenHeight;
+    int screenWidth;
+
+    // game state
+    boolean gameIsRunning;
+
+    // threading
+    Thread gameThread;
+
+
+    // drawing variables
+    SurfaceHolder holder;
+    Canvas canvas;
+    Paint paintbrush;
+
+Player player;
+
+    // -----------------------------------
+    // GAME SPECIFIC VARIABLES
+    // -----------------------------------
+
+    // ----------------------------
+    // ## SPRITES
+    // ----------------------------
+
+    // represent the TOP LEFT CORNER OF THE GRAPHIC
+
+    // ----------------------------
+    // ## GAME STATS
+    // ----------------------------
+
+
+    public GameEngine(Context context, int w, int h) {
+        super(context);
+
+        this.holder = this.getHolder();
+        this.paintbrush = new Paint();
+
+        this.screenWidth = w;
+        this.screenHeight = h;
+
+        this.printScreenInfo();
+
+
+        this.player = new Player(getContext(), 1500,500 );
+    }
+
+
+
+    private void printScreenInfo() {
+
+        Log.d(TAG, "Screen (w, h) = " + this.screenWidth + "," + this.screenHeight);
+    }
+
+    private void spawnPlayer() {
+        //@TODO: Start the player at the left side of screen
+    }
+    private void spawnEnemyShips() {
+        Random random = new Random();
+
+        //@TODO: Place the enemies in a random location
+
+    }
+
+    // ------------------------------
+    // GAME STATE FUNCTIONS (run, stop, start)
+    // ------------------------------
+    @Override
+    public void run() {
+        while (gameIsRunning == true) {
+            this.updatePositions();
+            this.redrawSprites();
+            this.setFPS();
+        }
+    }
+
+
+    public void pauseGame() {
+        gameIsRunning = false;
+        try {
+            gameThread.join();
+        } catch (InterruptedException e) {
+            // Error
+        }
+    }
+
+    public void startGame() {
+        gameIsRunning = true;
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+
+    // ------------------------------
+    // GAME ENGINE FUNCTIONS
+    // - update, draw, setFPS
+    // ------------------------------
+
+    public void updatePositions() {
+
+        if (this.fingerAction == "Tophalf") {
+            // if mousedown, then move player up
+            // Make the UP movement > than down movement - this will
+            // make it look like the player is moving up alot
+
+
+            player.setyPosition(200);
+            player.setxPosition(1500);
+           // player.updateHitbox();
+        }
+        else if (this.fingerAction == "Bottomhalf") {
+            // if mouseup, then move player down
+            player.setyPosition(400);
+            player.setxPosition(1500);
+           // player.updateHitbox(1500);
+        }
+
+
+
+
+    }
+
+    public void redrawSprites() {
+        if (this.holder.getSurface().isValid()) {
+            this.canvas = this.holder.lockCanvas();
+
+            //----------------
+
+            // configure the drawing tools
+            this.canvas.drawColor(Color.argb(255,255,255,255));
+            paintbrush.setColor(Color.WHITE);
+
+
+            canvas.drawBitmap(player.getImage(), player.getxPosition(), player.getyPosition(), paintbrush);
+            canvas.drawRect();
+
+
+
+            // DRAW THE PLAYER HITBOX
+            // ------------------------
+            // 1. change the paintbrush settings so we can see the hitbox
+            paintbrush.setColor(Color.BLUE);
+            paintbrush.setStyle(Paint.Style.STROKE);
+            paintbrush.setStrokeWidth(5);
+
+            //----------------
+            this.holder.unlockCanvasAndPost(canvas);
+        }
+    }
+
+    public void setFPS() {
+        try {
+            gameThread.sleep(120);
+        }
+        catch (Exception e) {
+
+        }
+    }
+
+    // ------------------------------
+    // USER INPUT FUNCTIONS
+    // ------------------------------
+
+
+    String fingerAction = "";
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int userAction = event.getActionMasked();
+        //@TODO: What should happen when person touches the screen?
+        if (userAction == MotionEvent.ACTION_DOWN)
+        {
+
+                fingerAction = "Tophalf";
+            } else if (userAction == MotionEvent.ACTION_UP)
+            {
+                fingerAction="Bottomhalf";
+            }
+
+
+
+            return true;
+        }
+    }
